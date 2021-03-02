673f6a0c064ea0192cf2ee5ecf653cd0c0da1309
sagar <poudelsagar530@gmail.com>
2021-03-02 12:05:46 -0600

    modify photos file

--- src/main/java/com/enservsolutions/dig/repository/PhotoRepository.java
+++ src/main/java/com/enservsolutions/dig/repository/PhotoRepository.java
@@ -1,11 +1,10 @@
 package com.enservsolutions.dig.repository;
 
 import com.enservsolutions.dig.entity.Photo;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface PhotoRepository extends JpaRepository<Photo, Integer> {
+public interface PhotoRepository extends CrudRepository<Photo, Integer> {
     Optional<Photo> findByName(String name);
 }
